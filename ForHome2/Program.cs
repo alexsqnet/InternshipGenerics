e10d61a858f112a2b35ae6c6e613600c5b4e9ad0
Alexandr Sipco <alexandr.sco@gmail.com>
2020-08-16 12:16:39 +0300

    Add ForHome2

---
+++ ForHome2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForHome2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 332 / 4.8;
+            int n = 90;
+            bool b = true;
+
+            Show(d);
+            Show(n);
+            Show(b);
+
+            Console.ReadLine();
+        }
+
+        static void Show(object input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
